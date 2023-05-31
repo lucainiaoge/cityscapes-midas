e71699365b8ca94b973eff1841af1b6e020cdae4
lucainiaoge <32015775+lucainiaoge@users.noreply.github.com>
2023-05-31 17:43:32 +0200

    Init

---
+++ cityscapes_loader.py
@@ -0,0 +1,235 @@
+# https://github.com/meetps/pytorch-semseg/blob/master/ptsemseg/utils.py
+
+"""
+Misc Utility functions
+"""
+import os
+import logging
+import datetime
+import numpy as np
+
+import cv2
+import torch
+import scipy.misc as m
+from torch.utils import data
+
+from collections import OrderedDict
+
+
+def recursive_glob(rootdir=".", suffix=""):
+    """Performs recursive glob with given suffix and rootdir
+        :param rootdir is the root directory
+        :param suffix is the suffix to be searched
+    """
+    return [
+        os.path.join(looproot, filename)
+        for looproot, _, filenames in os.walk(rootdir)
+        for filename in filenames
+        if filename.endswith(suffix)
+    ]
+
+
+def alpha_blend(input_image, segmentation_mask, alpha=0.5):
+    """Alpha Blending utility to overlay RGB masks on RBG images
+        :param input_image is a np.ndarray with 3 channels
+        :param segmentation_mask is a np.ndarray with 3 channels
+        :param alpha is a float value
+    """
+    blended = np.zeros(input_image.size, dtype=np.float32)
+    blended = input_image * alpha + segmentation_mask * (1 - alpha)
+    return blended
+
+
+def convert_state_dict(state_dict):
+    """Converts a state dict saved from a dataParallel module to normal
+       module state_dict inplace
+       :param state_dict is the loaded DataParallel model_state
+    """
+    if not next(iter(state_dict)).startswith("module."):
+        return state_dict  # abort if dict is not a DataParallel model_state
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = k[7:]  # remove `module.`
+        new_state_dict[name] = v
+    return new_state_dict
+
+
+def get_logger(logdir):
+    logger = logging.getLogger("ptsemseg")
+    ts = str(datetime.datetime.now()).split(".")[0].replace(" ", "_")
+    ts = ts.replace(":", "_").replace("-", "_")
+    file_path = os.path.join(logdir, "run_{}.log".format(ts))
+    hdlr = logging.FileHandler(file_path)
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+    hdlr.setFormatter(formatter)
+    logger.addHandler(hdlr)
+    logger.setLevel(logging.INFO)
+    return logger
+
+
+# https://github.com/meetps/pytorch-semseg/blob/master/ptsemseg/loader/cityscapes_loader.py
+
+class CityscapesDataset(data.Dataset):
+    """CityscapesDataset
+
+    https://www.cityscapes-dataset.com
+
+    Data is derived from CityScapes, and can be downloaded from here:
+    https://www.cityscapes-dataset.com/downloads/
+
+    Many Thanks to @fvisin for the loader repo:
+    https://github.com/fvisin/dataset_loaders/blob/master/dataset_loaders/images/cityscapes.py
+    """
+
+    colors = [  # [  0,   0,   0],
+        [128, 64, 128],
+        [244, 35, 232],
+        [70, 70, 70],
+        [102, 102, 156],
+        [190, 153, 153],
+        [153, 153, 153],
+        [250, 170, 30],
+        [220, 220, 0],
+        [107, 142, 35],
+        [152, 251, 152],
+        [0, 130, 180],
+        [220, 20, 60],
+        [255, 0, 0],
+        [0, 0, 142],
+        [0, 0, 70],
+        [0, 60, 100],
+        [0, 80, 100],
+        [0, 0, 230],
+        [119, 11, 32],
+    ]
+
+    label_colours = dict(zip(range(19), colors))
+
+    mean_rgb = {
+        "pascal": [103.939, 116.779, 123.68],
+        "cityscapes": [0.0, 0.0, 0.0],
+    }  # pascal mean for PSPNet and ICNet pre-trained model
+
+    def __init__(
+        self,
+        root,
+        split="train",
+        version="cityscapes",
+    ):
+        """__init__
+
+        :param root:
+        :param split:
+        """
+        self.root = root
+        self.split = split
+        self.n_classes = 19
+        self.mean = np.array(self.mean_rgb[version])
+        self.files = {}
+
+        self.images_base = os.path.join(self.root, "leftImg8bit", self.split)
+        self.annotations_base = os.path.join(self.root, "gtFine", self.split)
+
+        self.files[split] = recursive_glob(rootdir=self.images_base, suffix=".png")
+
+        self.void_classes = [0, 1, 2, 3, 4, 5, 6, 9, 10, 14, 15, 16, 18, 29, 30, -1]
+        self.valid_classes = [
+            7,
+            8,
+            11,
+            12,
+            13,
+            17,
+            19,
+            20,
+            21,
+            22,
+            23,
+            24,
+            25,
+            26,
+            27,
+            28,
+            31,
+            32,
+            33,
+        ]
+        self.class_names = [
+            "unlabelled",
+            "road",
+            "sidewalk",
+            "building",
+            "wall",
+            "fence",
+            "pole",
+            "traffic_light",
+            "traffic_sign",
+            "vegetation",
+            "terrain",
+            "sky",
+            "person",
+            "rider",
+            "car",
+            "truck",
+            "bus",
+            "train",
+            "motorcycle",
+            "bicycle",
+        ]
+
+        self.ignore_index = 250
+        self.class_map = dict(zip(self.valid_classes, range(19)))
+
+        if not self.files[split]:
+            raise Exception("No files for split=[%s] found in %s" % (split, self.images_base))
+
+        print("Found %d %s images" % (len(self.files[split]), split))
+
+    def __len__(self):
+        """__len__"""
+        return len(self.files[self.split])
+
+    def __getitem__(self, index):
+        """__getitem__
+
+        :param index:
+        """
+        img_path = self.files[self.split][index].rstrip()
+        lbl_path = os.path.join(
+            self.annotations_base,
+            img_path.split(os.sep)[-2],
+            os.path.basename(img_path)[:-15] + "gtFine_labelIds.png",
+        )
+
+        img = cv2.imread(filename)
+        img = cv2.cvtColor(img_midas, cv2.COLOR_BGR2RGB)
+        img = np.array(img, dtype=np.uint8)
+
+        lbl = m.imread(lbl_path)
+        lbl = self.encode_segmap(np.array(lbl, dtype=np.uint8))
+
+        return img, lbl, img_path
+
+
+    def decode_segmap(self, temp):
+        r = temp.copy()
+        g = temp.copy()
+        b = temp.copy()
+        for l in range(0, self.n_classes):
+            r[temp == l] = self.label_colours[l][0]
+            g[temp == l] = self.label_colours[l][1]
+            b[temp == l] = self.label_colours[l][2]
+
+        rgb = np.zeros((temp.shape[0], temp.shape[1], 3))
+        rgb[:, :, 0] = r / 255.0
+        rgb[:, :, 1] = g / 255.0
+        rgb[:, :, 2] = b / 255.0
+        return rgb
+
+    def encode_segmap(self, mask):
+        # Put all void classes to zero
+        for _voidc in self.void_classes:
+            mask[mask == _voidc] = self.ignore_index
+        for _validc in self.valid_classes:
+            mask[mask == _validc] = self.class_map[_validc]
+        return mask
---
+++ midas_cityscapes.py
@@ -0,0 +1,51 @@
+import torch
+from cityscapes_loader import CityscapesDataset
+
+class MidasHybrid(object):
+    def __init__(self):
+        self.model_type = "DPT_Hybrid"
+        self.midas = torch.hub.load("intel-isl/MiDaS", self.model_type)
+        self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.midas.to(self.device)
+        self.midas.eval()
+        self.midas_transform = torch.hub.load("intel-isl/MiDaS", "transforms").dpt_transform
+
+    def pred_depth_map(img):
+        img_torch = torch.from_numpy(img).to(self.device)
+        input_batch = self.midas_transform(img_torch).to(self.device)
+
+        with torch.no_grad():
+            prediction = self.midas(input_batch)
+
+            prediction = torch.nn.functional.interpolate(
+                prediction.unsqueeze(1),
+                size=img.shape[:2],
+                mode="bicubic",
+                align_corners=False,
+            ).squeeze()
+
+        return prediction.unsqueeze(-1).cpu().numpy() #(B, H, 1)
+
+if __name__ == "__main__":
+    local_path = "./data/cityscapes"
+    dataset_train = cityscapesLoader(local_path, split="train")
+    dataset_val = cityscapesLoader(local_path, split="val")
+    midas_predictor = MidasHybrid()
+
+    debug = True
+    debug_num = 10
+    debug_counter = 0
+    if not os.path.exists("debug"):
+        os.makedirs("debug")
+    for img, gt_seg, img_path in dataset_train:
+        print(img_path)
+        depth_map = midas_predictor.pred_depth_map(img) 
+        depth_map = depth_map / depth_map.max()
+
+        cv2.imwrite("./debug/{}_img_debug.png".format(debug_counter), img)
+        cv2.imwrite("./debug/{}_gt_seg_debug.png".format(debug_counter), gt_seg)
+        cv2.imwrite("./debug/{}_depth_map_debug.png".format(debug_counter), depth_map)
+
+        debug_counter += 1
+        if debug_counter > debug_num:
+            break
